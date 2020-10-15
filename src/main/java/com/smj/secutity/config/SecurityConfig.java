a0a7a7dac2b8ef13bb9a7b68bc61e46fd5f5936d
sumaojin <sumaojin@meb.com>
2020-10-15 18:09:26 +0800

    security学习，不断完善

---
+++ src/main/java/com/smj/secutity/config/SecurityConfig.java
@@ -0,0 +1,6 @@
+package com.smj.secutity.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
