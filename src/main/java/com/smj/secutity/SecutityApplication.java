fd61d5622bd339f314043cbc9b8411778bdad006
sumaojin <sumaojin@meb.com>
2020-10-15 17:51:14 +0800

    security学习，不断完善

---
+++ src/main/java/com/smj/secutity/SecutityApplication.java
@@ -0,0 +1,13 @@
+package com.smj.secutity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecutityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecutityApplication.class, args);
+    }
+
+}
